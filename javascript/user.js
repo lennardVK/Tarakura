5091bee6f6cb392b959992279cdb545a316543a5
Lennard Kollossa <l.kollossa@web.de>
2019-05-06 11:35:36 +0200

    wip

--- javascript/user.js
+++ javascript/user.js
@@ -1,22 +1,20 @@
 
+var database = firebase.database()
+let users = []
+
 class User {
   constructor(address, image) {
-    this.address = address;
-    this.image = image;
-  }
+    this.address = address
+    this.image = image
+  }dd
 }
 
 function createUser(){
-  let address = document.getElementById('address').value;
+  let address = document.getElementById('address').value
   let image = "assets/images/cathedral.jpg"
   let user = new User(address,image)
-
-  firebase.database().ref("User").set({
-    address: address,
-    image: image
-  });
- 
-
-  console.log("new User",user)
   
+  console.log('db',firebase.database().ref('/thegiveaway').child('thegiveaway'))
+  const dbRefObject = firebase.database().ref('/thegiveaway').push(user)
+  dbRefObject.on('value', snap => console.log(snap.val()))
 }
