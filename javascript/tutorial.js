2a8cee25b60f537f7a50ee74c30d6394bb9b2107
Lennard Kollossa <l.kollossa@web.de>
2019-05-26 15:32:40 +0200

    add cntrl logic

--- javascript/tutorial.js
+++ javascript/tutorial.js
@@ -1,5 +1,7 @@
 let wrapper = document.getElementsByClassName('tutorial')
 let elements = document.getElementsByClassName('tutorial_step')
+let rightCntrl = document.getElementById('right')
+let leftCntrl = document.getElementById('left')
 var arr = [].slice.call(elements);
 let width = elements[0].offsetWidth
 let index = 0
@@ -17,20 +19,35 @@ function moveRight(){
       obj.style.left = "" + newLeft + "vw"
     }) 
   }
-  
+  checkIndex()
 }
 
 function moveLeft(){
+  
   index = index - 100
   console.log(index)
   if (index <= -100){
     index = 0;
   }else {
-    console.log('moveLeft')
     arr.map(obj => {
       let currentLeft = parseInt(obj.style.left,10);
       let newLeft = currentLeft + 100
       obj.style.left = "" + newLeft + "vw"
     }) 
   }
+  checkIndex()
 }
+
+function checkIndex(){
+  if(index == 0){
+    
+    leftCntrl.style.display = "none"
+  } else if( index == 300) {
+    console.log('checked')
+    rightCntrl.style.display = "none"
+  } else{
+    leftCntrl.style.display = "block"
+    rightCntrl.style.display = "block"
+  }
+}
+checkIndex()
