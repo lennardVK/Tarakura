9c1293f26047cf9c0d532461a9b63794aa09fa90
Lennard Kollossa <l.kollossa@web.de>
2019-06-05 13:03:17 +0200

    wip

--- javascript/camera.js
+++ javascript/camera.js
@@ -1,25 +1,41 @@
 let preview = document.getElementById('preview')
 let imageElement = document.getElementById('image_element')
 let currentImgUrl = ""
+let frameBg = document.getElementById('bg')
 
 let currentId = localStorage.getItem('session')
 currentId = JSON.parse(currentId)[0].userId;
 currentId.toString()
 
-//let currentId = Math.floor(Math.random() * Math.floor(99999999999))
-console.log('currentId',currentId)
+let arrayOfBgs = ["https://www.colourbox.de/preview/5044200-feld-gras-textur.jpg",
+"https://images.unsplash.com/photo-1523295475921-ca5ea3d129a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2727&q=80",
+"https://images.unsplash.com/photo-1523295616404-2b6c098f26dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2300&q=80"
+]
 
-  
+let index = 0
+index = Math.floor(Math.random() * Math.floor(arrayOfBgs.length))
+
+let currentImg = arrayOfBgs[index]
+frameBg.src = currentImg 
+
+
 function getSnapshot(){
   
-  //let canvas = document.getElementById('mycanvas')
+  let canvas = document.getElementById('mycanvas')
   
-  //console.log(document.querySelector('a-scene').components.screenshot.capture('perspective'))
-  console.log(document.getElementsByTagName('video')[0])
   let video = document.querySelector('a-scene').components.screenshot.getCanvas('newCanvas')
   let dataURL = video.toDataURL('image/jpg')
+  context = canvas.getContext('2d');
+  
+  let width = canvas.width
+  let height = canvas.height
+
+  context.drawImage(video, 0, 0, width, height);
   currentImgUrl = dataURL
-  console.log('url',currentImgUrl)
+  console.log(currentImgUrl)
+  // Turn the canvas image into a dataURL that can be used as a src for our photo.
+  
+  
   /*
   let element = document.getElementById('screen')
   
@@ -29,13 +45,12 @@ function getSnapshot(){
   imageElement.src = dataURL  
   currentImgUrl = dataURL */
   showPreview()
+  
 }
 
 function delay (URL) {
   setTimeout( function() { window.location = URL }, 2000 );
 }
-
-
 
 function submit(URL){
   
@@ -54,9 +69,6 @@ function submit(URL){
   let blob = new Blob([ab], {type: mimeString})
   console.log('currentId: ',currentId)
   let storageRef = firebase.storage().ref(""+currentId+"")
-  
-
-
   
   storageRef.put(blob).then(function(snapshot) {
     console.log('Uploaded a blob or file!')
